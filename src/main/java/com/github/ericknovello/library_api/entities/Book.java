761d6a71438a791d4da1a3cbc5c93c0499c9cb76
Erick Novello <novello.erick@gmail.com>
2023-10-24 12:38:39 -0300

    Creating entities and exceptions

---
+++ src/main/java/com/github/ericknovello/library_api/entities/Book.java
@@ -0,0 +1,29 @@
+package com.github.ericknovello.library_api.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String autor;
+
+    private String isbn;
+}
---
+++ src/main/java/com/github/ericknovello/library_api/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.github.ericknovello.library_api.exception;
+
+public class BusinessException extends RuntimeException{
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
